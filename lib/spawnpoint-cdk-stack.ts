6df672945de0e19c8e8e19d101fb8787291dca3e
Aneesh-Pothuru <pothuruaneesh23@gmail.com>
2024-03-28 00:34:16 -0700

    Amplify CDK setup with GH Action CICD

--- lib/spawnpoint-cdk-stack.ts
+++ lib/spawnpoint-cdk-stack.ts
@@ -1,16 +1,28 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import { Construct } from '@aws-cdk/core';
+import * as amplify from '@aws-cdk/aws-amplify';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export class SpawnpointCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const app = new amplify.App(this, 'spawnpoint-aneesh-io', {
+      appName: 'spawnpoint',
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: 'aneesh.io',
+        repository: 'spawnpoint',
+        oauthToken: cdk.SecretValue.secretsManager('spawnpoint-aneesh-io-github-token'),
+      }),
+      basicAuth: amplify.BasicAuth.fromCredentials('spawnpoint-aneesh-io', cdk.SecretValue.secretsManager('spawnpoint-aneesh-io-amplify-password')),
+      customRules: [],
+      environmentVariables: {
+        ENV: 'production',
+      },
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SpawnpointCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const mainBranch = app.addBranch('main');
   }
 }
