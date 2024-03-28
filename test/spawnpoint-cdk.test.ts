a59c92ceaf9f8d095f8232cd60f224053d5dab69
Aneesh-Pothuru <pothuruaneesh23@gmail.com>
2024-03-27 22:51:21 -0700

    Setting up spawnpoint CDK

---
+++ test/spawnpoint-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SpawnpointCdk from '../lib/spawnpoint-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/spawnpoint-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SpawnpointCdk.SpawnpointCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
